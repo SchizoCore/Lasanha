33ba9ab61d717d6ee192419622e339d7bb3496ab
SchizoCore <73rayaneh@gmail.com>
2025-05-03 16:27:07 +0200

    Update Loader.lua

--- Scripts/Loader.lua
+++ Scripts/Loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 15784744207 then
-  loadstring(game:HttpGet("https://github.com/r7pha/Lasanha/raw/refs/heads/main/Scripts/ValleyPrison.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/Lasanha/refs/heads/main/Scripts/ValleyPrison.lua"))()
 end
